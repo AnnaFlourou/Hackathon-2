ef1c80e52725e5199a92ef75f092985aa7a7f2b7
jordanLeveque <jojodu28240@gmail.com>
2018-05-03 17:10:32 +0200

    menu integrate and ts, css

--- client/app/menu/menu.component.ts
+++ client/app/menu/menu.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  position = 'closed';
+  buttonMenu = 'but0';
+  menu = 'invisible';
   constructor() { }
 
   ngOnInit() {
   }
-
+  pos() {
+    if (this.position == 'closed') {
+      this.position = 'open';
+      this.buttonMenu = 'but1';
+      this.menu = 'visible';
+    } else {
+      this.position = 'closed';
+      this.buttonMenu = 'but0';
+      this.menu = 'invisible';
+    }
+    console.log(this.position);
+  }
 }

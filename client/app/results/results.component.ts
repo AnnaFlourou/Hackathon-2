396c16f39468ef690f45567312cee4d3a49adacb
Jo-zTk <jonathan.zietek@gmail.com>
2018-05-04 02:56:17 +0200

    Feat: Add score regarding days

--- client/app/results/results.component.ts
+++ client/app/results/results.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ScoreService } from '../services/score.service';
 import { Score } from '../shared/models/score.model';
+import { DayService } from '../services/day.service';
+import { Day } from '../shared/models/day.model';
 
 @Component({
   selector: 'app-results',
@@ -11,18 +13,30 @@ export class ResultsComponent implements OnInit {
 
   score = new Score();
   scores: Score[] = [];
+  days: any;
+
   isLoading = true;
 
 
-  constructor(private scoreService: ScoreService) { }
+  constructor(private scoreService: ScoreService,
+              private dayService: DayService) { }
 
   ngOnInit() {
     this.getScores();
+    this.getDays();
   }
 
   getScores() {
     this.scoreService.getScores().subscribe(
       data => this.scores = data,
+      error => console.log(error),
+      () => this.isLoading = false,
+    );
+  }
+
+  getDays() {
+    this.dayService.getDays().subscribe(
+      data => this.days = data[0].day,
       error => console.log(error),
       () => this.isLoading = false,
     );
